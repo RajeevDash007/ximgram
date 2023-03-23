4fe562fd4bf95831e039ad83968f8a6f5d0e873a
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-03-23 19:04:13 +0530

    added authentication

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,59 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+
+/*Register*/
+export const register = async (req,res) => {
+    try{
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation,
+        } = req.body;
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password: passwordHash,
+            picturePath,
+            friends,
+            location,
+            occupation,
+            viewedProfile: Mathfloor(Math.random() * 10000),
+            impressions: Mathfloor(Math.random() * 10000),
+        });
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    }catch(err){
+        res.status(500).json({error: err.message});
+    }
+}
+
+/* logging in */
+export const login = async (req, res) => {
+    try{
+        const {email,password} = req.body;
+        const user = await User.findOne({email: email});
+
+        if(!user) return res.status(400).json({msg: "User does not exist. "});
+
+        const isMatch = await bcrypt.compare(password,user.password);
+
+        if(!isMatch) return res.status(400).json({msg: 'Invalid Credential'});
+        delete user.password;
+        res.status(200).json({token,user});
+
+        const token = jwt.sign({ id: user._id},"IamRajTheCoder");
+    }catch(err){
+        res.status(500).json({error: err.message});
+    }
+}
--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
 import { register } from "./controllers/auth.js";
 
 /*config*/
@@ -39,6 +40,8 @@ const upload = multer({ storage });
 /*Routes with files*/
 app.post("/auth/register",upload.single("picture"), register);
 
+/*Routes*/
+app.use("/auth",authRoutes);
 /* mongoose setup */
 const PORT = 3001 || 6001;
 mongoose.connect('mongodb+srv://dummyuser:Rajeev0057@cluster0.poel0rb.mongodb.net/?retryWrites=true&w=majority',{
