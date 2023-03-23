36a22b67b907d3769f1231c809eb86d52599a927
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-03-23 14:07:04 +0530

    setup the react env and backend config

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+/*config*/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
+app.use(morgan("morgan"));
+app.use(bodyParser.json({limit:"30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+app.use("/assets",express.static(path.join(__dirname,'public/assets')));
+
+
+/*file Storage*/
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,"public/assets");
+    },
+    filename : function(req,file,cb){
+        cb(null, file.originalname);
+    }
+});
+const upload = multer({ storage });
