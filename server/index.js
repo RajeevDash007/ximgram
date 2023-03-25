a1ccd436880bb27e7b2e26fa7cef8d577f6046b7
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-03-25 14:14:40 +0530

    User routes added

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { register } from "./controllers/auth.js";
 
 /*config*/
@@ -42,6 +43,7 @@ app.post("/auth/register",upload.single("picture"), register);
 
 /*Routes*/
 app.use("/auth",authRoutes);
+app.use("/users",userRoutes);
 /* mongoose setup */
 const PORT = 3001 || 6001;
 mongoose.connect('',{
