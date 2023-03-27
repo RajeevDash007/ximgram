f539daf96fd9b9ab1399be3d7c46aa90e038f0d9
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-03-27 11:30:47 +0530

    added different pages config

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 
-import './App.css';
+import {BrowseRouter, Navigate, Routes, Route} from 'react-router-dom';
+import HomePage from 'scenes/homePage';
+import LoginPage from 'scenes/loginPage';
+import Navbar from 'scenes/navbar';
+import ProfilePage from 'scenes/profilePage';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowseRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowseRouter>
     </div>
   );
 }
