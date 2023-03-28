08a20a1b68a92b0a726a24361d82c3fd3d502c94
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-03-28 12:45:41 +0530

    created the navbar

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,5 +1,9 @@
+import { Box } from "@mui/material";
+import Navbar from "scenes/navbar";
 const HomePage = () => {
-    return(<div>HomePage</div>)
+    return <Box> 
+        <Navbar />
+    </Box>
 }
 
 export default HomePage;
