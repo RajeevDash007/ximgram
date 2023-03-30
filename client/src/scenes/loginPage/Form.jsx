ce831f2ab1ec3fbd8148ede01f30e18498b8347c
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-03-30 10:55:44 +0530

    added login function

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -81,7 +81,25 @@ const Form = () => {
           setPageType("login");
         }
       };
-      
+      const login = async (values, onSubmitProps) => {
+        const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(values),
+        });
+        const loggedIn = await loggedInResponse.json();
+        onSubmitProps.resetForm();
+        if (loggedIn) {
+          dispatch(
+            setLogin({
+              user: loggedIn.user,
+              token: loggedIn.token,
+            })
+          );
+          navigate("/home");
+        }
+      };
+    
 
     return(
         <Formik 
