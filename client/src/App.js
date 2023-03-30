424f3f4ec743632b06ad2fdb357b4943a9dcf277
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-03-30 13:09:16 +0530

    updated router and secured the DB

--- client/src/App.js
+++ client/src/App.js
@@ -21,8 +21,8 @@ function App() {
         <CssBaseline />
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route path="/home" element={isAuth ? <HomePage /> : <Navigate to="/" /> } />
+            <Route path="/profile/:userId" element={isAuth ? <ProfilePage /> : <Navigate to="/" />} />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
