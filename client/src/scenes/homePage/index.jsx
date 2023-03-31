0b2453d03239417eb1ba4cd818ef1530a1aecb07
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-03-31 11:28:34 +0530

    Home Page profile division created

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,8 +1,25 @@
-import { Box } from "@mui/material";
+
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
+import UserWidget from "scenes/widgets/UserWidget";
+
 const HomePage = () => {
+    const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+    const { _id, picturePath } = useSelector((state) => state.user);
     return <Box> 
         <Navbar />
+            <Box
+            width="100%"
+            padding="2rem 6%"
+            display={isNonMobileScreens ? "flex" : "block"}
+            gap="0.5rem"
+            justifyContent="space-between"
+            >
+                <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+                    <UserWidget userId={_id} picturePath={picturePath} />
+                </Box>
+            </Box>
     </Box>
 }
 
