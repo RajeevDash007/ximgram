93a6af2f351cf34d61466be4659bbb9044ef05d7
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-04-01 10:28:24 +0530

    Added handlePost function

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -3,6 +3,7 @@ import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
 
 const HomePage = () => {
     const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -18,6 +19,13 @@ const HomePage = () => {
             >
                 <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
                     <UserWidget userId={_id} picturePath={picturePath} />
+                </Box>
+                <Box
+                    flexBasis={isNonMobileScreens ? "42%" : undefined}
+                    mt={isNonMobileScreens ? undefined : "2rem"}
+                >
+                    <MyPostWidget picturePath={picturePath} />
+                    
                 </Box>
             </Box>
     </Box>
---
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -0,0 +1,53 @@
+import {
+    EditOutlined,
+    DeleteOutlined,
+    AttachFileOutlined,
+    GifBoxOutlined,
+    ImageOutlined,
+    MicOutlined,
+    MoreHorizOutlined,
+  } from "@mui/icons-material";
+  import {
+    Box,
+    Divider,
+    Typography,
+    InputBase,
+    useTheme,
+    Button,
+    IconButton,
+    useMediaQuery,
+  } from "@mui/material";
+  import FlexBetween from "components/FlexBetween";
+  import Dropzone from "react-dropzone";
+  import UserImage from "components/UserImage";
+  import WidgetWrapper from "components/WidgetWrapper";
+  import { useState } from "react";
+  import { useDispatch, useSelector } from "react-redux";
+  import { setPosts } from "state";
+
+  
+  const MyPostWidget = ({picturePath}) => {
+    
+    const dispatch = useDispatch();
+    const [isImage, setIsImage] = useState(false);
+    const [image, setImage] = useState(null);
+    const [post, setPost] = useState("");
+    const { palette } = useTheme();
+    const { _id } = useSelector((state) => state.user);
+    const token = useSelector((state) => state.token);
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+    const mediumMain = palette.neutral.mediumMain;
+    const medium = palette.neutral.medium;
+
+    const handlePost = async () => {
+        const formData = new FormData();
+        formData.append("userId", _id);
+        formData.append("description", post);
+        if (image) {
+            formData.append("picture", image);
+            formData.append("picturePath", image.name);
+        }
+    }
+  }
+
+  export default MyPostWidget;
