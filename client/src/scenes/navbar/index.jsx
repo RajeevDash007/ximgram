46a02b8cc75ba105471e15acfbea97b6bf0490fe
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-04-02 08:30:37 +0530

    changes in navbar

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
-      <FlexBetween gap="1.75rem">
+      <FlexBetween gap="18rem">
         <Typography
           fontWeight="bold"
           fontSize="clamp(1rem, 2rem, 2.25rem)"
